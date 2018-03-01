1caf5e57e41394ed69421f02c5b464fda543f884
Kamil Kowalski <kamkow93@gmail.com>
2018-03-01 20:42:39 +0100

    Sout edited

--- src/me/jurec/Main.java
+++ src/me/jurec/Main.java
@@ -2,8 +2,10 @@ package me.jurec;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println(calculateTheAnswer());
+        System.out.println("Answer is:" + calculateTheAnswer());
+
     }
 
     public static int calculateTheAnswer() {
@@ -19,4 +21,6 @@ public class Main {
     private static int multiply(int a, int b) {
         return 0;
     }
+
+
 }
